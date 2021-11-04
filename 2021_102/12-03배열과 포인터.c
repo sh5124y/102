4f7596765b5d6333df97ab73eca72f7792555f90
sohee1214 <82165798+sohee1214@users.noreply.github.com>
2021-11-04 12:00:59 +0900

    arr[1]과 *(arr+1)과 *&arr[1]은 같다

--- 2021_102/12-03배열과 포인터.c
+++ 2021_102/12-03배열과 포인터.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 
-
 void main(void)
 {
-	int arr[4];
+	int arr[4] = { 10, 20, 30, 40 };
 	// 0번째 공간의 주소
 	printf("&arr[0]	: %p\n", &arr[0]);
-	printf("arr	: %p\n", arr);
+	printf("arr	: %p\n\n", arr);
 
-	//0번째 공간의 주소 
-	printf("*arr[0]	: %d\n", arr[0]);
-	printf("*arr	: %p\n", *arr);
+	// 0번째 공간의 요소(element)값
+	printf("arr[0]	: %d\n", arr[0]);
+	printf("*arr	: %d\n\n", *arr);
 
 	// 1번째 공간의 주소
 	printf("&arr[1]	: %p\n", &arr[1]);
 	printf("arr+1	: %p\n\n", arr + 1);
 
-	char str[4]="cut";
+	// 1번째 공간의 요소(element)값
+	printf("arr[1]	: %d\n", arr[1]);
+	printf("*(arr+1): %d\n\n", *(arr + 1));
 
+	char str[4] = "cut";
 	// 0번째 공간의 주소
 	printf("&str[0]	: %p\n", &str[0]);
-	printf("str	: %p\n", str);
+	printf("str	: %p\n\n", str);
 
 	// 1번째 공간의 주소
 	printf("&str[1]	: %p\n", &str[1]);
-	printf("str+1	: %p\n", str + 1);
+	printf("str+1	: %p\n\n", str + 1);
 }
